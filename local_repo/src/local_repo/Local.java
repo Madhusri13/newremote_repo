3e9557f2988018c488bff7ebcb6ecc32cabd584c
670332 <670332@PC286526.cts.com>
2018-01-04 10:23:51 +0530

    changes in remote repo

--- local_repo/src/local_repo/Local.java
+++ local_repo/src/local_repo/Local.java
@@ -6,10 +6,6 @@ public class Local {
 		System.out.println("print local repo");
 
 	}
-<<<<<<< HEAD
-//changes in local repo
-=======
-	//edited in remote repo
->>>>>>> branch 'master' of https://github.com/Madhusri13/newremote_repo
+
 
 }
